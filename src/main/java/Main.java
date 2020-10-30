7b8b38344549138f237d95e3086d93a387b421de
Vengardd <vengardd@users.noreply.github.com>
2020-10-30 11:24:57 +0100

    Added event listener for home path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,44 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("aa");
+
+        Path homePath = Path.of("G:\\InteliJWorkspace\\prietius-task\\HOME");
+        Path devStringPath = Path.of("G:\\InteliJWorkspace\\prietius-task\\DEV");
+        Path testStringPath = Path.of("G:\\InteliJWorkspace\\prietius-task\\TEST");
         try {
-            String homePath = "G:\\InteliJWorkspace\\prietius-task\\HOME";
             createDirectoryIfDoesntExists(homePath);
-            String devPath = "G:\\InteliJWorkspace\\prietius-task\\DEV";
-            createDirectoryIfDoesntExists(devPath);
-            String testPath = "G:\\InteliJWorkspace\\prietius-task\\TEST";
-            createDirectoryIfDoesntExists(testPath);
+            createDirectoryIfDoesntExists(devStringPath);
+            createDirectoryIfDoesntExists(testStringPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        WatchService watchService
+                = FileSystems.getDefault().newWatchService();
+
+        homePath.register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
+
+        WatchKey key;
+        while ((key = watchService.take()) != null) {
+            for (WatchEvent<?> event : key.pollEvents()) {
+                System.out.println(
+                        "Event kind:" + event.kind()
+                                + ". File affected: " + event.context() + ".");
+            }
+            key.reset();
+        }
     }
 
-    private static void createDirectoryIfDoesntExists(String path) throws IOException {
-        Path homePath = Path.of(path);
-        if(!Files.exists(homePath)) {
-            Files.createDirectory(homePath);
+    private static void createDirectoryIfDoesntExists(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            Files.createDirectory(path);
         }
     }
 }
